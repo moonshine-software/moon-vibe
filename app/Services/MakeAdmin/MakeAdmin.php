02cd0e1f63958fef3842d4f4b05e4780fa54f193
levchenko-ivan <ivan97111@yandex.ru>
2025-06-24 23:14:29 +0300

    fix: gitignore

--- app/Services/MakeAdmin/MakeAdmin.php
+++ app/Services/MakeAdmin/MakeAdmin.php
@@ -68,6 +68,7 @@ readonly class MakeAdmin
             ->optimizeClear(77)
             ->migrateFresh(84)
             ->chmod(95)
+            ->removeGit()
         ;
 
         return config('app.url') . '/generate';
@@ -118,6 +119,37 @@ readonly class MakeAdmin
     private function chmod(int $percent): self
     {
         $this->alert(__('app.build.chmod'), $percent);
+
+        return $this->runProcess(
+            ['chmod', '-R', '775', $this->directories->appProjectDirectory . '/storage'],
+            'Failed chmod',
+            $this->directories->appProjectDirectory
+        );
+    }
+
+    private function removeGit(): self
+    {
+        $gitPath = $this->directories->appProjectDirectory . '/.git';
+        if (is_dir($gitPath)) {
+            $this->runProcess(
+                ['rm', '-rf', $gitPath],
+                'Failed to remove git directory'
+            );
+
+            $this->runProcess(
+                ['rm', $this->directories->appProjectDirectory . '/.gitignore'],
+                'Failed to remove .gitignore'
+            );
+
+            $this->runProcess(
+                [
+                    'cp',
+                    $this->directories->appProjectDirectory . '/../storage/logs/.gitignore',
+                    $this->directories->appProjectDirectory . '/.gitignore'
+                ],
+                'Failed to copy .gitignore'
+            );
+        }
 
         return $this->runProcess(
             ['chmod', '-R', '775', $this->directories->appProjectDirectory . '/storage'],

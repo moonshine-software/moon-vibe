535d727044ba5d83b1e1d45d24265b3b963f0d41
levchenko-ivan <ivan97111@yandex.ru>
2025-06-28 17:16:45 +0300

    fix: tests

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Foundation\Testing\TestCase;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}

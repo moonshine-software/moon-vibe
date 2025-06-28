6aa75286489a074e2f129948ba1b29d6e399c920
levchenko-ivan <ivan97111@yandex.ru>
2025-06-28 14:59:51 +0300

    refactor: fix docker

--- app/Services/MakeAdmin/Operations/PrepareDirectories.php
+++ app/Services/MakeAdmin/Operations/PrepareDirectories.php
@@ -6,18 +6,14 @@ class PrepareDirectories extends AbstractMakeOperation
 {
     public function handle(int $percent): void
     {
-        if(! is_dir($this->directories->path)) {
-            return;
+        if(is_dir($this->directories->path)) {
+            $this->runProcess(
+                ['rm', '-rf', $this->directories->path],
+                'Failed to remove directory'
+            );
         }
 
-        $this->runProcess(
-            ['rm', '-rf', $this->directories->path],
-            'Failed to remove directory'
-        );
-
-        if(! is_dir($this->directories->path)) {
-            mkdir($this->directories->path, recursive: true);
-        }
+        mkdir($this->directories->path, recursive: true);
 
         if(! is_dir($this->directories->schemaDirectory)) {
             mkdir($this->directories->schemaDirectory);
